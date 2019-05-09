67bad758e9d75bcaf5a00b21d3fb1c49e545554f
0LL1 <o.a.h.rautiainen@gmail.com>
2019-05-10 00:28:29 +0300

    Cleaner total

--- kurssitiedot/src/Total.js
+++ kurssitiedot/src/Total.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
 const Total = ({ parts }) => {
-  const exercises = parts.map(part => part.exercises)
-
-  const total = exercises.reduce(
-    (exercise, currentValue) => exercise + currentValue
+  const total = parts.reduce(
+    (accumulator, part) => accumulator + part.exercises,
+    0
   )
 
   return <p>Yhteensä {total} tehtävää</p>
