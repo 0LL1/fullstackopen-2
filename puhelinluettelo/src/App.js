f6e8c68cafdf95a4e5a3f5850a8fb5f2d3cf53ad
0LL1 <o.a.h.rautiainen@gmail.com>
2019-05-16 20:07:37 +0300

    2.19-2.20

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -3,12 +3,16 @@ import personService from './services/persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Notification from './components/Notification'
+import Error from './components/Error'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+  const [message, setMessage] = useState(null)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     personService.getAll().then(initialPersons => setPersons(initialPersons))
@@ -50,6 +54,7 @@ const App = () => {
       setPersons(persons.concat(returnedEntry))
       setNewName('')
       setNewNumber('')
+      setMessage(`${returnedEntry.name} lisätty`)
     })
   }
 
@@ -60,6 +65,7 @@ const App = () => {
       personService.remove(item.id)
       const newPersons = persons.filter(person => person.id !== item.id)
       setPersons(newPersons)
+      setMessage(`${item.name} poistettu`)
     }
   }
 
@@ -68,13 +74,18 @@ const App = () => {
 
     const newEntry = { ...oldEntry, number: item.number }
 
-    personService.replace(newEntry).then(returnedEntry => {
-      setPersons(
-        persons.map(person =>
-          person.id !== returnedEntry.id ? person : returnedEntry
+    personService
+      .replace(newEntry)
+      .then(returnedEntry => {
+        setPersons(
+          persons.map(person =>
+            person.id !== returnedEntry.id ? person : returnedEntry
+          )
         )
-      )
-    })
+        setMessage(`${item.name} muutettu`)
+      })
+      // I guess this is all that is needed in 2.20*
+      .catch(setError(`${item.name} oli jo poistettu`))
   }
 
   const filteredPersons = persons.filter(person =>
@@ -102,6 +113,8 @@ const App = () => {
       />
       <h2>Numerot</h2>
       <Persons persons={personList} />
+      <Notification message={message} setMessage={setMessage} />
+      <Error error={error} setError={setError} />
     </>
   )
 }
